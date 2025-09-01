8903762d08a498204a0a82afe08c429550538b7c
ramkumar2098 <2098ramkumar@gmail.com>
2025-09-01 21:45:54 +0530

    trying to print the secret

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ console.log('hello world from learn github actions')
 console.log('DB_URL', process.env.DB_URL)
 console.log('JWT_SECRET', process.env.JWT_SECRET)
 console.log('MY_SECRET', process.env.MY_SECRET)
+
+console.log('print secret', +process.env.DB_URL + +process.env.JWT_SECRET)
