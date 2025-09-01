407038a581b822dd0e3d05237d9018c49988e973
ramkumar2098 <2098ramkumar@gmail.com>
2025-09-01 21:05:53 +0530

    trying out envs

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 console.log('hello world from learn github actions')
 
-console.log('new log line')
+console.log('DB_URL', process.env.DB_URL)
+console.log('JWT_SECRET', process.env.JWT_SECRET)
+console.log('MY_SECRET', process.env.MY_SECRET)
