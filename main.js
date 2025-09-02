341dde970e12832414095a9d06b2a0e3682a2a7a
ramkumar2098 <2098ramkumar@gmail.com>
2025-09-02 11:34:20 +0530

    update envs

--- main.js
+++ main.js
@@ -4,18 +4,9 @@ const app = express()
 
 app.get('/', (req, res) => {
   console.log('you have hit /')
-  res.send(
-    'hello there' +
-      process.env.DB_URL +
-      process.env.JWT_SECRET +
-      process.env.MY_SECRET
-  )
+  res.send('hello there' + process.env.DB_URL + process.env.JWT_SECRET)
 })
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
-
-// adding comment to test new docker image build
-// 1
-// 2
