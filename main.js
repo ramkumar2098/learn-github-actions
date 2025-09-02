afa80091fb86dcb1d01c84ab2087f73bff9c8d8c
ramkumar2098 <2098ramkumar@gmail.com>
2025-09-02 20:50:06 +0530

    trying ci with prettier

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
   console.log('you have hit /')
